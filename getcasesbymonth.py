b2d9695e4430e50bdb4fa48957072426300135ad
James Pothen <69491494+jamespothen@users.noreply.github.com>
2025-08-20 12:44:13 -0400

    starts refactor of monthly totals calculation

--- getcasesbymonth.py
+++ getcasesbymonth.py
@@ -3,11 +3,29 @@ import re
 infile = open("./csv/Charlotin-hallucination_cases.csv", "r")
 outfile = open("./csv/monthlytotals.csv", "w")
 
+#TODO: get month count dict to have format of "January 2023" instead of "01-2023"
+monthnamesdict = {}
+
+monthcountdict = {}
+
 for line in infile:
     linecontentsarray = line.split(',')
     for str in linecontentsarray:
-        if str[0:4] == "2023" or str[0:4] == "2024" or str[0:4] == "2025":
-            print(f"{str[5:7]}-{str[0:4]}", file=outfile)
+        year_str = str[0:4]
+        if year_str == "2023" or year_str == "2024" or year_str == "2025":
+            month_str = str[5:7]
+            monthandyearstr = month_str + "-" + year_str
+
+            print(f"{monthandyearstr}")
+
+            if monthandyearstr in monthcountdict:
+                monthcountdict[monthandyearstr] += 1
+            else:
+                monthcountdict[monthandyearstr] = 1
+
+            print(f"{month_str}-{year_str}", file=outfile)
+
+print(monthcountdict)
 
 infile.close()
 outfile.close()
